19de13b4b2cf9bc3319e5bec71156b5a87e800ed
dc-ops <dc-ops@terraform.hub>
2026-06-01 14:23:50 +1000

    updated new code for ansible and terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,7 +96,7 @@ resource "aws_instance" "web" {
   associate_public_ip_address = true
 
   root_block_device {
-    volume_size           = 20
+    volume_size           = 30
     volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
