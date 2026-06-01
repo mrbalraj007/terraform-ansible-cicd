6f14bc4e7fcd9bf165280f2ad32cdfdbe548e7e2
dc-ops <dc-ops@terraform.hub>
2026-06-01 14:32:25 +1000

    Increase app instance root volume to 30GB for AL2023 AMI

--- terraform/main.tf
+++ terraform/main.tf
@@ -122,7 +122,7 @@ resource "aws_instance" "app" {
   associate_public_ip_address = true
 
   root_block_device {
-    volume_size           = 20
+    volume_size           = 30
     volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
