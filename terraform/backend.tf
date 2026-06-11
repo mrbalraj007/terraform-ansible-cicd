0ea8a0011c42eb75e3b13eb964690daa50734b0f
Your Name <you@example.com>
2026-06-11 11:57:18 +1000

    update new code

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-oidc-terraform-ansible-cicd-20260611"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "aws-oidc-terraform-ansible-cicd-20260611"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
