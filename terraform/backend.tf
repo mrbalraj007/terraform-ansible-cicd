9d0e8c476843cb5ba437626989544ae748f9ead5
Your Name <you@example.com>
2026-06-11 11:54:06 +1000

    updated new code

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,9 @@
-# Backend is configured via CLI flags in the workflow:
-#   terraform init -backend-config=bucket=$TF_VAR_tf_state_bucket
-##   -backend-config=key=terraform.tfstate
-#   -backend-config=region=$AWS_REGION
-# The empty backend "s3" {} block signals intent to use S3, so Terraform
-# doesn't warn when -backend-config is passed at init time.
-
-###############################################################################
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "aws-oidc-terraform-ansible-cicd-20260611"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
 }
